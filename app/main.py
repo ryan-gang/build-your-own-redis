d9711c2e48316fd70b1aeb8bcf443d003f72330a
Ryan Gang <ryan-gg@outlook.com>
2023-12-19 22:28:11 +0530

    Stage 3. Blindly Respond to multiple PINGs.

--- app/main.py
+++ app/main.py
@@ -6,8 +6,10 @@ HOST, PORT = "localhost", 6379
 def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
     conn, _ = server_socket.accept()  # wait for client
-    _ = conn.recv(1024)
-    conn.send("+PONG\r\n".encode())
+    while True:
+        data = conn.recv(1024)
+        if data:
+            conn.sendall("+PONG\r\n".encode())
     conn.close()
 
 
