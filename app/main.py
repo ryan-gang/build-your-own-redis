e9a3822858cabe2682d9a6747b1591e44bb1ec9c
Your Name <you@example.com>
2023-12-19 17:18:00 +0000

    CLI tests (cli-test-1703006280472)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
-import socket
+import asyncio
+from asyncio import StreamReader, StreamWriter
 
 HOST, PORT = "localhost", 6379
 
 
-def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept()  # wait for client
+async def handler(reader: StreamReader, writer: StreamWriter):
+    while 1:
+        data = await reader.read(1024)
+        if not data:
+            break
+        writer.write("+PONG\r\n".encode())
+        await writer.drain()
+    writer.write_eof()
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(handler, HOST, PORT, reuse_port=True)
+
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Interrupted, shutting down.")
