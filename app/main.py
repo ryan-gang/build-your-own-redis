ad403ce55d4df4e7a7a6f949b8a7352ae0e47829
Ryan Gang <ryan-gg@outlook.com>
2023-12-19 22:11:13 +0530

    Stage 1. Bind to a port.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-# Uncomment this to pass the first stage
-# import socket
+import socket
+
+HOST, PORT = "localhost", 6379
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    server_socket.accept()  # wait for client
 
 
 if __name__ == "__main__":
