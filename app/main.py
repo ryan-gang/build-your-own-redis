77365bc156ad93997e9e0badfcae2f03950ff94a
Your Name <you@example.com>
2023-12-19 16:57:38 +0000

    CLI tests (cli-test-1703005058394)

--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ HOST, PORT = "localhost", 6379
 
 def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, _ = server_socket.accept()  # wait for client
+    while True:
+        data = conn.recv(1024)
+        if data:
+            conn.sendall("+PONG\r\n".encode())
+    conn.close()
 
 
 if __name__ == "__main__":
