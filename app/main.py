0d3053e8498b6bd6b3c72429e3d324f862a9b6a3
Your Name <you@example.com>
2023-12-20 09:05:44 +0000

    CLI tests (cli-test-1703063144312)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,59 @@
-import socket
+import asyncio
+from asyncio import IncompleteReadError, StreamReader, StreamWriter
 
-HOST, PORT = "localhost", 6379
+from app.active import (EXPIRY_TIMESTAMP_DEFAULT_VAL, actively_expire_keys,
+                        check_key_expiration, get_expiry_timestamp)
+from app.resp import RESPReader, RESPWriter
+
+HOST, PORT = "127.0.0.1", 6379
+ACTIVE_KEY_EXPIRY_TIME_WINDOW = 60  # seconds
+DATASTORE: dict[str, tuple[str, int]] = {}  # key -> (value, expiry_timestamp)
 
 
-def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept()  # wait for client
+async def handler(stream_reader: StreamReader, stream_writer: StreamWriter):
+    reader, writer = RESPReader(stream_reader), RESPWriter(stream_writer)
+    while 1:
+        try:
+            msg = await reader.read_message()
+        except IncompleteReadError:
+            return
+        print(msg)
+        command = msg[0].upper()
+        match command:
+            case "PING":
+                response = "PONG"
+                await writer.write_simple_string(response)
+            case "ECHO":
+                response = msg[1]
+                await writer.write_bulk_string(response)
+            case "SET":
+                key, value = msg[1], msg[2]
+                DATASTORE[key] = (value, get_expiry_timestamp(msg))
+                await writer.write_simple_string("OK")
+            case "GET":
+                key = msg[1]
+                default_value = (None, EXPIRY_TIMESTAMP_DEFAULT_VAL)
+                value, expiry_timestamp = DATASTORE.get(key, default_value)
+                expired = check_key_expiration(DATASTORE, key, expiry_timestamp)
+                if expired:
+                    value = None
+                await writer.write_bulk_string(value)
+            case _:
+                raise RuntimeError(f"Unknown command received : {command}")
+
+
+async def main():
+    server = await asyncio.start_server(handler, HOST, PORT, reuse_port=False)
+    print(f"Started Redis server @ {HOST}:{PORT}")
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+        asyncio.create_task(
+            actively_expire_keys(DATASTORE, ACTIVE_KEY_EXPIRY_TIME_WINDOW)
+        )
+    except KeyboardInterrupt:
+        print("Interrupted, shutting down.")
