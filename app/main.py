00f68378613484295b84ce6f4b9f3410b2ea078a
Your Name <you@example.com>
2023-12-20 07:54:20 +0000

    CLI tests (cli-test-1703058860641)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,40 @@
-import socket
+import asyncio
+from asyncio import StreamReader, StreamWriter, IncompleteReadError
 
-HOST, PORT = "localhost", 6379
+from app.resp import RESPReader, RESPWriter
+
+HOST, PORT = "127.0.0.1", 6379
 
 
-def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept()  # wait for client
+async def handler(stream_reader: StreamReader, stream_writer: StreamWriter):
+    reader, writer = RESPReader(stream_reader), RESPWriter(stream_writer)
+    while 1:
+        try:
+            msg = await reader.read_message()
+        except IncompleteReadError:
+            return
+        command = msg[0].upper()
+        match command:
+            case "PING":
+                response = "PONG"
+                await writer.write_simple_string(response)
+            case "ECHO":
+                response = msg[1]
+                await writer.write_bulk_string(response)
+            case _:
+                raise RuntimeError(f"Unknown command received : {command}")
+        print(msg)
+
+
+async def main():
+    server = await asyncio.start_server(handler, HOST, PORT, reuse_port=False)
+    print(f"Started Redis server @ {HOST}:{PORT}")
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Interrupted, shutting down.")
---
+++ app/resp.py
@@ -0,0 +1,112 @@
+from asyncio import StreamReader, StreamWriter
+from typing import Any, Optional
+
+
+class RESPReader(object):
+    def __init__(self, reader: StreamReader):
+        self.reader = reader
+
+    async def read_message(self) -> Any:
+        msg_code = (await self.reader.readexactly(1)).decode()
+        match msg_code:
+            case "+":  # Simple String
+                string = await self.read_simple_string()
+                return string
+            case "-":  # Simple Error
+                err = await self.read_simple_error()
+                return err
+            case ":":  # Integer
+                num = await self.read_integer()
+                return num
+            case "$":  # Bulk String
+                string = await self.read_bulk_string()
+                return string
+            case "*":  # Array
+                arr = await self.read_array()
+                return arr
+            case _:
+                raise RuntimeError(f"Unknown payload identifier : {msg_code}")
+
+    async def read_array(self) -> Optional[list[Any]]:
+        arr: list[Any] = []
+
+        metadata = await self.read_line()
+        if not metadata:
+            return arr
+        length = int(metadata)
+        if length == -1:
+            return None
+        for _ in range(length):
+            msg = await self.read_message()
+            arr.append(msg)
+        return arr
+
+    async def read_simple_string(self) -> str:
+        data = await self.read_line()
+        return data
+
+    async def read_simple_error(self) -> str:
+        data = await self.read_line()
+        return data
+
+    async def read_integer(self) -> int:
+        data = await self.read_line()
+        return int(data)
+
+    async def read_bulk_string(self) -> Optional[str]:
+        metadata = await self.read_line()
+        print(metadata)
+        length = int(metadata)
+        if length == -1:
+            return None
+        string = await self.read_until(length + 2)
+        # We can't use readuntil(b"\r\n) over here, cause the content might
+        # contain our seperator, and we would lose the data. Instead we need to
+        # use the length to get the full data.
+        return string
+
+    async def read_line(self) -> str:
+        data = await self.reader.readuntil(b"\r\n")
+        print(data)
+        return data[:-2].decode()
+
+    async def read_until(self, n: int) -> str:
+        data = await self.reader.readexactly(n)
+        print(data)
+        return data[:-2].decode()
+
+
+class RESPWriter(object):
+    def __init__(self, writer: StreamWriter):
+        self.writer = writer
+
+    async def write_simple_string(self, data: str):
+        MSG_CODE, DELIMITER = "+", "\r\n"
+        message = MSG_CODE + data + DELIMITER
+        await self.write(message)
+
+    async def write_simple_error(self, data: str):
+        MSG_CODE, DELIMITER = "-", "\r\n"
+        message = MSG_CODE + data + DELIMITER
+        await self.write(message)
+
+    async def write_integer(self, data: int):
+        MSG_CODE, DELIMITER = ":", "\r\n"
+        message = MSG_CODE + str(data) + DELIMITER
+        await self.write(message)
+
+    async def write_bulk_string(self, data: Optional[str]):
+        MSG_CODE, DELIMITER = "$", "\r\n"
+        if not data:  # Null bulk strings
+            message = "$-1\r\n"
+        else:
+            message = MSG_CODE + str(len(data)) + DELIMITER + data + DELIMITER
+        await self.write(message)
+
+    async def write(self, message: str):
+        self.writer.write(message.encode())
+        await self.writer.drain()
+
+    async def close(self):
+        self.writer.write_eof()
+        self.writer.close()
