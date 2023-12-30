c6714f900365f687038d5ba7cbb9557a8966bef7
Your Name <you@example.com>
2023-12-30 17:15:47 +0000

    CLI tests (cli-test-1703956547017)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,96 @@
-import socket
+import argparse
+import asyncio
+import os
+from asyncio import IncompleteReadError, StreamReader, StreamWriter
 
-HOST, PORT = "localhost", 6379
+from app.commands import (
+    handle_config_get,
+    handle_echo,
+    handle_get,
+    handle_list_keys,
+    handle_ping,
+    handle_set,
+)
+from app.expiry import actively_expire_keys
+from app.rdb import RDBParser
+from app.resp import RESPReader, RESPWriter
+
+HOST, PORT = "127.0.0.1", 6379
+ACTIVE_KEY_EXPIRY_TIME_WINDOW = 60  # seconds
+DATASTORE: dict[str, tuple[str, int]] = {}  # key -> (value, expiry_timestamp)
+# Main Datastore, All SET, GET data is stored in this global dict.
+CONFIG: dict[str, str] = {}  # key -> value (Redis config parameters)
+rdb_parser_required = False
 
 
-def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept()  # wait for client
+async def handler(stream_reader: StreamReader, stream_writer: StreamWriter):
+    """
+    Handles incoming RESP commands from Redis clients and interacts with the
+    application logic. This asynchronous function continuously performs the
+    following tasks in a loop:
+    * Reads a single RESP message from the `stream_reader` using the
+      `RESPReader`.
+    * Handles potential read errors by closing the connection and returning.
+    * Extracts the first element of the parsed message as the command name
+      (converted to uppercase).
+    * Matches the command name to known handlers, and executes the handler
+      function to perform the required operation.
+    """
+    reader, writer = RESPReader(stream_reader), RESPWriter(stream_writer)
+    while not stream_reader.at_eof():
+        try:
+            msg = await reader.read_message()
+            print(msg)
+        except (IncompleteReadError, ConnectionResetError):
+            await writer.close()
+            return
+        command = msg[0].upper()
+        if rdb_parser_required:
+            rdb_parser = RDBParser(rdb_file_path)
+            DATASTORE = rdb_parser.kv
+        match command:
+            case "PING":
+                await handle_ping(writer)
+            case "ECHO":
+                await handle_echo(writer, msg)
+            case "SET":
+                await handle_set(writer, msg, DATASTORE)
+            case "GET":
+                await handle_get(writer, msg, DATASTORE)
+            case "CONFIG":
+                await handle_config_get(writer, msg, CONFIG)
+            case "KEYS":
+                await handle_list_keys(writer, msg, DATASTORE)
+            case _:
+                raise RuntimeError(f"Unknown command received : {command}")
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dir", type=str, help="The directory where RDB files are stored"
+    )
+    parser.add_argument("--dbfilename", type=str, help="The name of the RDB file")
+    args = parser.parse_args()
+
+    if args.dir and args.dbfilename:
+        global rdb_file_path, rdb_parser_required
+        CONFIG["dir"] = dir = str(args.dir)
+        CONFIG["dbfilename"] = filename = str(args.dbfilename)
+        rdb_parser_required = True
+        rdb_file_path = os.path.join(dir, filename)
+
+    server = await asyncio.start_server(handler, HOST, PORT, reuse_port=False)
+    print(f"Started Redis server @ {HOST}:{PORT}")
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+        asyncio.create_task(
+            actively_expire_keys(DATASTORE, ACTIVE_KEY_EXPIRY_TIME_WINDOW)
+        )
+    except KeyboardInterrupt:
+        print("Interrupted, shutting down.")
