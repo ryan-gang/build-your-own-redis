993dfcf535bf8de6ecbd95472e561c865b97f1fa
Ryan Gang <ryan-gg@outlook.com>
2024-02-12 11:46:46 +0000

    CLI tests (cli-test-1707738406102)

--- app/replication.py
+++ app/replication.py
@@ -1,4 +1,4 @@
-from asyncio import StreamReader, StreamWriter, sleep
+from asyncio import StreamReader, StreamWriter, sleep, Lock
 from collections import deque
 
 from app.resp import RESPReader, RESPWriter
@@ -8,27 +8,29 @@ async def replication_handshake(
     stream_reader: StreamReader, stream_writer: StreamWriter
 ):
     """ """
+    lock = Lock()
     reader, writer = RESPReader(stream_reader), RESPWriter(stream_writer)
 
-    ping = ["PING"]
-    await writer.write_array(ping)
-    data = await reader.read_simple_string()
-    print(data)
+    async with lock:
+        ping = ["PING"]
+        await writer.write_array(ping)
+        data = await reader.read_simple_string()
+        print(data)
 
-    replconf = ["REPLCONF", "listening-port", "6380"]
-    await writer.write_array(replconf)
-    data = await reader.read_simple_string()
-    print(data)
+        replconf = ["REPLCONF", "listening-port", "6380"]
+        await writer.write_array(replconf)
+        data = await reader.read_simple_string()
+        print(data)
 
-    replconf_capa = ["REPLCONF", "capa", "psync2", "capa", "psync2"]
-    await writer.write_array(replconf_capa)
-    data = await reader.read_simple_string()
-    print(data)
+        replconf_capa = ["REPLCONF", "capa", "psync2", "capa", "psync2"]
+        await writer.write_array(replconf_capa)
+        data = await reader.read_simple_string()
+        print(data)
 
-    replconf_capa = ["PSYNC", "?", "-1"]
-    await writer.write_array(replconf_capa)
-    data = await reader.read_simple_string()
-    print(data)
+        replconf_capa = ["PSYNC", "?", "-1"]
+        await writer.write_array(replconf_capa)
+        data = await reader.read_simple_string()
+        print(data)
 
     return
 
